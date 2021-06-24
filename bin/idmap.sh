9f5d5a1abc073dc80283d7bae0292470e0853abc
Zane van Iperen <z.vaniperen@uq.edu.au>
2021-06-24 21:57:19 +1000

    fix shell script

--- bin/idmap.sh
+++ bin/idmap.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-export GLOBUS_IDMAP_CONFIG_PATH=/opt/uq-globus-tools/etc/idmap.json
-exec /opt/globus/bin/python3 /opt/uq-globus-tools/bin/idmap.py
+
+BASEDIR=/opt/uq-globus-tools
+PYTHON=/opt/globus/bin/python3
+
+#BASEDIR="$HOME/Documents/Coding/globus-uidmap"
+#PYTHON=python
+
+export GLOBUS_IDMAP_CONFIG_PATH="$BASEDIR/etc/config.json"
+export PYTHONPATH="$BASEDIR/vendor:$PYTHONPATH"
+exec $PYTHON "$BASEDIR/bin/idmap.py" $@
