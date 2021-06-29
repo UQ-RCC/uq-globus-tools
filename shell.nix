9bf0644acba617f53c48b1bbf27bb0349987ef96
Zane van Iperen <z.vaniperen@uq.edu.au>
2021-06-29 17:09:17 +1000

    nix: use a version of selinux compatible with rhel 8

--- shell.nix
+++ shell.nix
@@ -1,7 +1,25 @@
 { pkgs ? (import <nixpkgs> {}), stdenv ? pkgs.stdenv }:
+let
+  xlibsepol = pkgs.libsepol.overrideDerivation(old: rec {
+    inherit (old) se_url;
+    se_release = "20190315";
+    version    = "2.9";
+
+    src = pkgs.fetchurl {
+      url = "${se_url}/${se_release}/libsepol-${version}.tar.gz";
+      sha256 = "0p8x7w73jn1nysx1d7416wqrhbi0r6isrjxib7jf68fi72q14jx3";
+    };
+
+  });
+in
 stdenv.mkDerivation {
   pname   = "uq-globus-tools-devshell";
   version = "1.0.0";
 
-  nativeBuildInputs = with pkgs; [ rpm gnumake ];
+  nativeBuildInputs = with pkgs; [
+    rpm
+    gnumake
+    (checkpolicy.override { libsepol = xlibsepol; })
+    semodule-utils
+  ];
 }
